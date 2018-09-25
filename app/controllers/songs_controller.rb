d1b6f40b1abcba516ca50da2deb3bfe7d434f0b0
Scott Weinstein <sweinstein27@gmail.com>
2018-09-25 17:44:48 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,16 @@
 class SongsController < ApplicationController
   def index
-      @songs = Artist.find(params[:artist_id]).songs
-    rescue ActiveRecord::RecordNotFound
-      flash[:notice] = "ID not found"
-      redirect_to artists_path
-  end
+   if params[:artist_id]
+     @artist = Artist.find_by(id: params[:artist_id])
+     if @artist.nil?
+       redirect_to artists_path, alert: "Artist not found"
+     else
+       @songs = @artist.songs
+     end
+   else
+     @songs = Song.all
+   end
+ end
 
   def show
     @song = Song.find(params[:id])
